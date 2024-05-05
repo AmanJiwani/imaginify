f3f443f2a58f138fd3c81c41429ea2b015ddfa49
AmanJiwani <140176190+AmanJiwani@users.noreply.github.com>
2024-05-05 17:48:38 +0530

    Update transaction.action.ts

--- lib/actions/transaction.action.ts
+++ lib/actions/transaction.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
 import Stripe from "stripe";
 import { handleError } from '../utils';
 import { connectToDatabase } from '../database/mongoose';
@@ -33,9 +33,9 @@ export async function checkoutCredits(transaction: CheckoutTransactionParams) {
     mode: 'payment',
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-  })
+  });
 
-  redirect(session.url!)
+  redirect(session.url!);
 }
 
 export async function createTransaction(transaction: CreateTransactionParams) {
@@ -45,12 +45,12 @@ export async function createTransaction(transaction: CreateTransactionParams) {
     // Create a new transaction with a buyerId
     const newTransaction = await Transaction.create({
       ...transaction, buyer: transaction.buyerId
-    })
+    });
 
     await updateCredits(transaction.buyerId, transaction.credits);
 
     return JSON.parse(JSON.stringify(newTransaction));
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
